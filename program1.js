93b1ad161c583c22713bd6eecaeda4d00e2652d4
Sriram0612458 <121300942+Sriram0612458@users.noreply.github.com>
2024-11-11 13:24:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -9,26 +9,22 @@ const getTotalIsles = function (map) {
   let islandCount = 0;
 
   const dfs = (row, col) => {
-      // Check for out of bounds
       if (row < 0 || row >= rows || col < 0 || col >= cols) return;
 
-      // Check if the current cell is water or already visited
+
       if (map[row][col] === 'W' || visited[row][col]) return;
 
-      // Mark the landmass as visited
       visited[row][col] = true;
 
-      // Explore all four directions (up, down, left, right)
-      dfs(row - 1, col); // Up
-      dfs(row + 1, col); // Down
-      dfs(row, col - 1); // Left
-      dfs(row, col + 1); // Right
+      dfs(row - 1, col); 
+      dfs(row + 1, col); 
+      dfs(row, col - 1); 
+      dfs(row, col + 1);
   };
 
   for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
           if (map[i][j] === 'L' && !visited[i][j]) {
-              // Found an unvisited landmass, start DFS
               dfs(i, j);
               islandCount++;
           }
